53eedbadf572cce6627674de10272e7155ef6c87
Sierra iOS SME <lsanche1@gmail.com>
2026-06-09 16:28:38 -0400

    fix(meta): restore /100 in tip() (systematic-debugging: 1500 vs 15 = x100)

--- Sources/SpikeApp/TipCalculator.swift
+++ Sources/SpikeApp/TipCalculator.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum TipCalculator {
     static func tip(bill: Double, percent: Double) -> Double {
-        bill * percent
+        bill * percent / 100
     }
 
     static func total(bill: Double, percent: Double) -> Double {
